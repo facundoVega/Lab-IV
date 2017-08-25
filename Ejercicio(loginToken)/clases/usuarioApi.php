9daecda39df150b97356a2fb01b54c0788062ae2
facundo <horacio_facundo.vega@hotmail.com>
2017-08-25 08:33:28 -0300

    Ejercicio de loguin de usuarios si es correcto se le devuelve un token sino un mensaje de error

---
+++ Ejercicio(loginToken)/clases/usuarioApi.php
@@ -0,0 +1,116 @@
+<?php
+require_once 'usuario.php';
+require_once 'IApiUsable.php';
+require_once 'AutentificadorJWT.php';
+
+class usuarioApi extends usuario implements IApiUsable
+{
+ 	public function TraerUno($request, $response, $args) {
+     	$id=$args['id'];
+    	$eluser=usuario::TraerUnUsuario($id);
+     	$newResponse = $response->withJson($eluser, 200);  
+        $response->getBody()->write("<h1>TraerUno</h1>");
+    	return $newResponse;
+    }
+     public function TraerTodos($request, $response, $args) {
+      	$todosLosUsuarios=usuario::TraerTodoLosUsuarios();
+		 $response = $response->withJson($todosLosUsuarios, 200);  
+
+		 $response->getBody()->write("traer todos");
+    	 return $response;
+    }
+      public function CargarUno($request, $response, $args) {
+     	$ArrayDeParametros = $request->getParsedBody();
+        //var_dump($ArrayDeParametros);
+        $nombre= $ArrayDeParametros['nombre'];
+        $tipo= $ArrayDeParametros['tipo'];
+      
+        
+        $miuser = new usuario();
+		$miuser->nombre=$nombre;
+		$miuser->tipo=$tipo;
+     
+        
+        $miuser->InsertarElUsuarioParametros();
+        //$archivos = $request->getUploadedFiles();
+        //$destino="./fotos/";
+        //var_dump($archivos);
+        //var_dump($archivos['foto']);
+       // $nombreAnterior=$archivos['foto']->getClientFilename();
+        //$extension= explode(".", $nombreAnterior)  ;
+        //var_dump($nombreAnterior);
+       // $extension=array_reverse($extension);
+        //$archivos['foto']->moveTo($destino.$titulo.".".$extension[0]);
+        $response->getBody()->write("se guardo el user");
+		return $response;
+		
+	}
+	public function ExisteUsuario($request, $response, $args)
+	{
+		$ArrayDeParametros = $request->getParsedBody();
+
+		$nombre  = $ArrayDeParametros["nombre"];
+		$pass = $ArrayDeParametros["pass"];
+		$miuser = new usuario();
+		$usuarioBuscado=null;
+		$usuarioBuscado = $miuser->BuscarUsuario($pass, $nombre);
+		
+
+		if($usuarioBuscado!=null)
+		{
+			$token=AutentificadorJWT::CrearToken($usuarioBuscado);
+			$response->getBody()->write($token);
+			return $response;
+		}
+		else
+		{
+			$response->getBody()->write("no es un usuario de la api");
+			return $response;
+		}
+
+		$response->getBody()->write($nombre." "." "." ".  $pass);
+		return $response;
+
+	}
+      public function BorrarUno($request, $response, $args) {
+		 $ArrayDeParametros = $request->getParsedBody();
+		 var_dump($ArrayDeParametros);
+		 $id=$ArrayDeParametros['id'];
+     	$user= new usuario();
+     	$user->id=$id;
+     	$cantidadDeBorrados=$user->BorrarUsuario();
+     	$objDelaRespuesta= new stdclass();
+	    $objDelaRespuesta->cantidad=$cantidadDeBorrados;
+	    if($cantidadDeBorrados>0)
+	    	{
+	    		 $objDelaRespuesta->resultado="algo borro!!!";
+	    	}
+	    	else
+	    	{
+	    		$objDelaRespuesta->resultado="no Borro nada!!!";
+	    	}
+		$newResponse = $response->withJson($objDelaRespuesta, 200);  
+		$response->getBody()->write("Borrar uno");
+      	return $newResponse;
+    }
+     
+     public function ModificarUno($request, $response, $args) {
+     	$response->getBody()->write("<h1>Modificar  uno</h1>");
+     	$ArrayDeParametros = $request->getParsedBody();
+	    var_dump($ArrayDeParametros);    	
+	    $miuser = new usuario();
+	    $miuser->id=$ArrayDeParametros['id'];
+	    $miuser->nombre=$ArrayDeParametros['nombre'];
+	    $miuser->tipo=$ArrayDeParametros['tipo'];
+	   
+	   	$resultado =$miuser->ModificarUsuarioParametros();
+	   	$objDelaRespuesta= new stdclass();
+		//var_dump($resultado);
+		$objDelaRespuesta->resultado=$resultado;
+		return $response->withJson($objDelaRespuesta, 200);	
+		
+		return $response;
+	
+
+    }
+}
---
+++ Ejercicio(loginToken)/index.php
@@ -0,0 +1,34 @@
+<?php
+    use \Psr\Http\Message\ServerRequestInterface as Request;
+    use \Psr\Http\Message\ResponseInterface as Response;
+    require_once 'vendor/autoload.php';
+    require_once '/clases/AccesoDatos.php';
+    require_once '/clases/usuario.php';
+    require_once '/clases/usuarioApi.php';
+    require_once '/clases/AutentificadorJWT.php';
+    require_once '/clases/MWparaCORS.php';
+    require_once '/clases/MWparaAutentificar.php';
+
+    $config['displayErrorDetails'] = true;
+    $config['addContentLengthHeader'] = false;
+
+    $app = new \Slim\App(["settings" => $config]);
+
+
+    $app->group('/usuario', function () {   
+        $this->get('/', usuarioApi::class . ':TraerTodos')->add(\MWparaCORS::class . ':HabilitarCORSTodos');;
+        $this->get('/{id}', \usuarioApi::class . ':traerUno')->add(\MWparaCORS::class . ':HabilitarCORSTodos');;
+        $this->delete('/', \usuarioApi::class . ':BorrarUno');
+        $this->put('/', \usuarioApi::class . ':ModificarUno');
+       $this->post('/', \usuarioApi::class . ':ExisteUsuario');
+      
+          
+            
+})->add(\MWparaAutentificar::class . ':VerificarUsuario')->add(\MWparaCORS::class . ':HabilitarCORS8080');;
+        
+
+
+
+    $app->run();
+
+
